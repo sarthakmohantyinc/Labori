538cfc0701f0311e80f74000b0aed47ed8464dc5
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-09-19 01:46:50 +0000

    ⚗ attempt to add markdown to pdf conversion

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const axios = require('axios').default;
+var markdownpdf = require("markdown-pdf");
 const AWS = require('aws-sdk');
 var s3 = new AWS.S3({
     apiVersion: '2006-03-01',
@@ -26,6 +27,7 @@ app.message(/.*/, async ({
     say
 }) => {
     console.log(message);
+    // Private Upload
     if (message.subtype === 'file_share' && message.channel === 'C017CP00UHW') {
         await app.client.files.sharedPublicURL({
                 token: process.env.SLACK_TOKEN,
@@ -167,7 +169,8 @@ app.message(/.*/, async ({
                     icon_emoji: ':cyclone:',
                 });
             });
-    } else if (message.subtype === 'file_share' && message.channel === 'C0157NF6T3P') {
+    } // Public Upload
+    else if (message.subtype === 'file_share' && message.channel === 'C0157NF6T3P') {
         await app.client.files.sharedPublicURL({
                 token: process.env.SLACK_TOKEN,
                 file: message.files[0].id
@@ -316,7 +319,8 @@ app.message(/.*/, async ({
                     icon_emoji: ':cyclone:',
                 });
             });
-    } else if (message.subtype === 'file_share' && message.channel === 'C01A1KXB8AJ') {
+    } // CDN
+    else if (message.subtype === 'file_share' && message.channel === 'C01A1KXB8AJ') {
         await app.client.files.sharedPublicURL({
                 token: process.env.SLACK_TOKEN,
                 file: message.files[0].id
@@ -453,7 +457,54 @@ app.message(/.*/, async ({
                     icon_emoji: ':cyclone:',
                 });
             });
-    } else {
+    } else if (message.subtype === 'file_share' && message.channel === 'C01ATHB7C6S') {
+        await app.client.files.sharedPublicURL({
+            token: process.env.SLACK_TOKEN,
+            file: message.files[0].id
+        }).then((res) => {
+            if (res.file.name.split('.').pop() === 'md') {
+                const slackUrlRegex = RegExp(/(?:https:\/\/slack\-files\.com)\/(.+)\-(.+)\-(.+)/i);
+                const slackFileLink = slackUrlRegex.exec(res.file.permalink_public);
+                const slackTeamId = slackFileLink[1];
+                const slackFileId = slackFileLink[2];
+                const slackFilePubSecret = slackFileLink[3];
+                const slackFileName = res.file.name.toLowerCase().replace(/[|&;$%@"<>#!'()*^+,\s]/g, "_");
+                const pubLink = `https://files.slack.com/files-pri/${slackTeamId}-${slackFileId}/${slackFileName}?pub_secret=${slackFilePubSecret}`
+                markdownpdf({
+                    cssPath: "https://stackedit.io/style.css"
+                }).from(pubLink).to("converted.pdf", function () {
+                    app.client.files.upload({
+                        token: process.env.SLACK_BOT_TOKEN,
+                        thread_ts: message.ts,
+                        file: 'converted.pdf',
+                        filename: slackFileName,
+                        filetype: 'pdf',
+                    });
+                    console.log(`Converted ${slackFileName} successfully!`)
+                });
+            } else {
+                await app.client.chat.delete({
+                    token: process.env.SLACK_TOKEN,
+                    channel: message.channel,
+                    ts: message.ts
+                }).catch((err) => {
+                    console.log(err);
+                });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            app.client.chat.postMessage({
+                token: process.env.SLACK_BOT_TOKEN,
+                channel: message.channel,
+                thread_ts: message.ts,
+                text: 'Failed to make yo\' file public. ERR: `' + err.data.error + '` now keep moving :car:, keep moving I said!',
+                username: 'the construction workers on i-10',
+                icon_emoji: ':cyclone:',
+            });
+        });
+    } // Delete all other messages
+    else {
         if (typeof message.thread_ts === 'undefined') {
             await app.client.chat.delete({
                 token: process.env.SLACK_TOKEN,
